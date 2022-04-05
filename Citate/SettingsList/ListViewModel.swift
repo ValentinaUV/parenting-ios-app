1e4933c427d4a5afa531d84731bf32d896d4ddfa
Valentina <galaxyspevv@gmail.com>
2022-04-05 10:31:27 +0300

    changes for combine implementation

--- Citate/SettingsList/ListViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ListViewModel.swift
-//  Citate
-//
-//  Created by Ungurean Valentina on 31.03.2022.
-//
-
-import Foundation
--- Citate/SettingsList/SettingsService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SettingsService.swift
-//  Citate
-//
-//  Created by Ungurean Valentina on 01.04.2022.
-//
-
-import Foundation
